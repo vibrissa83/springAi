d964fec73d37bacbc887a921a68988bd0bb48ad2
Eddy <vibrissa83@gmail.com>
2025-03-25 15:50:00 +0900

    어플리케이션 스타트 위치 수정

--- src/main/java/com/eddy/springAi/SpringAiApplication.java
+++ src/main/java/com/eddy/springAi/SpringAiApplication.java
@@ -1,9 +1,9 @@
-package com.eddy.springAi.cmm;
+package com.eddy.springAi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.eddy.springAi")
+@SpringBootApplication
 public class SpringAiApplication {
 
 	public static void main(String[] args) {
